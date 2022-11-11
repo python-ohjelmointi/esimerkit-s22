6897a0b3c5353203a47df2825db514d79efcab2f
Teemu H <44917122+swd1tn002@users.noreply.github.com>
2022-11-11 21:41:30 +0000

    Pelin hienosäätö

--- sudoku/sudoku_peli.py
+++ sudoku/sudoku_peli.py
@@ -1,11 +1,11 @@
 '''
 Tämä tiedosto sisältää Sudoku-pelin käyttöliittymän, mutta se ei sisällä lainkaan
-pelin logiikkaa. Logiikka on toteutettu aikaisemmissa viikkoharjoituksissa, ja
-lisäksi pelin tueksi on toteutettu uudet sudoku_valmis-, ja tulosta-funktiot.
+pelin logiikkaa. Logiikka on toteutettu aikaisemmissa viikkoharjoituksissa.
+Pelin tueksi on toteutettu lisäksi sudoku_valmis-niminen funktio.
 Logiikka löytyy tiedostosta sudoku_tarkistin.py.
 '''
 
-from sudoku_tarkistin import tulosta, sudoku_oikein, sudoku_valmis
+from sudoku_tarkistin import sudoku_oikein, sudoku_valmis
 
 
 # Sudoku-logo on luotu https://patorjk.com/software/taag/ -palvelussa.
@@ -19,29 +19,43 @@ logo = r'''
 '''
 
 
-def kysy_numero(teksti: str) -> int:
+def pelaa(ruudukko: list) -> bool:
     '''
-    Pyytää käyttäjää syöttämään kokonaisluvun käyttäen annettua tekstiä.
-    Toistaa kysymyksen kunnes saa kelvollisen kokonaisluvun, joka palautetaan.
+    Pelaa Sudokua annetulla 2-ulotteisella kokonaislukuruudukolla, kunnes
+    peli on läpäisty tai pelaaja keskeyttää pelin. 
+
+    Palauttaa True tai False riippuen siitä, pelattiinko peli loppuun.
     '''
 
-    # Toistetaan, kunnes saadaan kelvollinen numero:
+    print(logo)
+
+    # Toistetaan siirtoja, kunnes peli on valmis:
     while True:
-        try:
-            numero = int(input(teksti))
-            return numero
+        tulosta_ruudukko(ruudukko)
+        print()  # Tyhjä rivi ruudukon ja kysymysten väliin
 
-        except ValueError as virhe:
-            print('Virheellinen numero!')
-            print('Voit lopettaa pelin painamalla Ctrl+C')
-            print(virhe)
+        try:
+            pelaa_siirto(ruudukko)
+
+            if sudoku_valmis(ruudukko):
+                print('Sait sudokun valmiiksi 🥳:\n')
+                tulosta_ruudukko(ruudukko)
+                return True
+
+        except KeyboardInterrupt:
+            # Ctrl+C -komento johtaa pelistä poistumiseen
+            print('Kiitos pelaamisesta 👋!')
+            return False
+
+        print()  # Tyhjä rivi jokaisen siirron jälkeen
 
 
 def pelaa_siirto(ruudukko: list) -> None:
     '''
-    Kysyy pelaajalta siirron, ja koittaa tehdä sen annetussa ruudukossa.
+    Kysyy pelaajalta siirron ja tekee sen annetussa ruudukossa.
     Jos siirto ei ole sallittu, se perutaan.
     '''
+
     rivi = kysy_numero('Syötä rivi (0-8): ')
     sarake = kysy_numero('Syötä sarake (0-8): ')
     numero = kysy_numero('Syötä numero (1-9): ')
@@ -64,28 +78,68 @@ def pelaa_siirto(ruudukko: list) -> None:
         print('Virheellinen rivi tai sarake!')
 
 
-def pelaa(ruudukko: list):
-    print(logo)
+def kysy_numero(teksti: str) -> int:
+    '''
+    Pyytää käyttäjää syöttämään kokonaisluvun käyttäen annettua tekstiä.
+    Toistaa kysymyksen, kunnes saa kelvollisen kokonaisluvun, joka palautetaan.
+    '''
 
-    # Toistetaan siirtoja, kunnes peli on valmis:
-    while not sudoku_valmis(ruudukko):
-        tulosta(ruudukko)
-        print()
-
+    # Toistetaan kysymystä, kunnes saadaan kelvollinen numero:
+    while True:
         try:
-            pelaa_siirto(ruudukko)
+            numero = int(input(teksti))
+            return numero
 
-        except KeyboardInterrupt:
-            # Ctrl+C -komento johtaa pelistä poistumiseen
-            return False
+        except ValueError as virhe:
+            print('Virheellinen numero!')
+            print('Voit lopettaa pelin painamalla Ctrl+C')
+            print(virhe)
 
-        print()  # Tyhjä rivi jokaisen siirron jälkeen
 
-    return True
+def tulosta_ruudukko(ruudukko: list):
+    '''
+    Tulostaa annetun ruudukon siten, että rivit ja sarakkeet on numeroitu.
+    Eri "neliöt" sudokussa erotellaan putkilla (|) ja viivoilla (-), esim:
+
+        0 1 2   3 4 5   6 7 8
+
+    0   2 6 7 | 8 3 9 | 5   4
+    1   9 8 3 | 5 1   | 6   2
+    2   4 5 1 | 6     | 8 3 9
+        ------ ------- ------
+    3   5 1 9 |   4 6 | 3 2 8
+    4   8   2 | 1   5 | 7   6
+    5   6 7 4 | 3 2   |     5
+        ------ ------- ------
+    6         | 4 5 7 | 2 6 3
+    7   3 2   |   8   |   5 7
+    8   7 4 5 |     3 | 9   1
+    '''
+
+    ylin = '    0 1 2   3 4 5   6 7 8'
+    vali = '    ------ ------- ------'
+
+    print(ylin)
+    print()
+
+    for rivi_nro, rivi in enumerate(ruudukko):
+        if rivi_nro in (3, 6):
+            print(vali)
+
+        print(f'{rivi_nro}   ', end='')
+
+        for sarake_nro, numero in enumerate(rivi):
+            if sarake_nro in (3, 6):
+                print('| ', end='')
+
+            # Nollien tilalla näytetään vain välilyönti:
+            print(numero if numero != 0 else ' ', end=' ')
+
+        print()  # Rivinvaihto viimeisen sarakkeen jälkeen
 
 
 def main():
-    # pelin alkutila on lainattu mooc.fi:n esimerkistä
+    # Pelin alkutila on lainattu mooc.fi:n esimerkistä
     ruudukko = [
         [2, 6, 7, 8, 3, 9, 5, 0, 4],
         [9, 0, 3, 5, 1, 0, 6, 0, 0],
@@ -98,16 +152,8 @@ def main():
         [7, 4, 5, 0, 0, 3, 9, 0, 1]
     ]
 
-    valmis = pelaa(ruudukko)
-    print()
-
-    if valmis:
-        print('Sait sudokun valmiiksi 🥳:\n')
-        tulosta(ruudukko)
-
-    else:
-        print('Kiitos pelaamisesta 👋!')
+    pelaa(ruudukko)
 
 
 if __name__ == '__main__':
-    main()  # käynnistää pelin
+    main()
